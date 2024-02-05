13b27eb5b46b30a17a1d68cc5805556d08f99682
Sarthak Kashyap <98842340+ROOKIEsarthak@users.noreply.github.com>
2024-02-05 19:18:58 +0530

    talked about strings

---
+++ 06-strings.js
@@ -0,0 +1,11 @@
+const name = 'ambivert'
+const repoCount = 50
+//console.log(name + repoCount + " repos");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Ambivert1')
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.charAt(4));
