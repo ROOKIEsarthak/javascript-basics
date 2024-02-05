aacc292d2e1fca9c6f99d671d3d5bc8a213f1d49
Sarthak Kashyap <98842340+ROOKIEsarthak@users.noreply.github.com>
2024-02-05 16:05:23 +0530

    conversion of operators in javascript and prefix and postfix opeators

--- 03-conversion-of-datatypes.js
+++ 03-conversion-of-datatypes.js
@@ -3,3 +3,6 @@ let someNumber = 33;
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber));
+
+
+
---
+++ 04-operations-in-numbers-in-javascript.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+let str1 = 'hello'
+let str2 = ' ambivert'
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log('1'+2);
+console.log(1+'2');
+console.log('1'+2+2);
+console.log(1+2+'2');
+//console.log((3+4)*(5%3));
+
+/* 
+prefix and postfix operators.
+
+
+The ++ operator is overloaded for two types of operands: number and BigInt. 
+It first coerces the operand to a numeric value 
+and tests the type of it. It performs BigInt increment 
+if the operand becomes a BigInt; otherwise, 
+it performs number increment.
+
+If used postfix, with operator after 
+operand (for example, x++), the increment 
+operator increments and returns the value before incrementing.
+
+If used prefix, with operator before 
+operand (for example, ++x), the increment operator 
+increments and returns the value after incrementing.
+
+
+*/
